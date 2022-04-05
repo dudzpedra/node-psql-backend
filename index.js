9019552f46d125d55917c291d26f425bbd381457
Dudzpedra <poliveiraeduardo@gmail.com>
2022-04-05 03:17:47 -0300

    fixed headers in index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const merchant_model = require("./models/merchant");
 app.use(express.static('build'))
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "/");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
