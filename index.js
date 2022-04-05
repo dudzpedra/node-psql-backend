c2e3f2888c9eba9f15e82231e226d25946793448
Dudzpedra <poliveiraeduardo@gmail.com>
2022-04-05 02:39:23 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const config = require("./utils/config");
+
+const merchant_model = require("./models/merchant");
+
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
+  next();
+});
+
+app.get("/api/", async (req, res) => {
+    const response = await merchant_model.getMerchants()
+    try {
+        res.status(200).send(response);
+    } catch (error) {
+        res.status(500).send(error)
+    }
+});
+
+app.post('/api/merchants', async (req, res) => {
+    const response = await merchant_model.createMerchant(req.body)
+    try {
+        res.status(201).send(response)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+app.delete('/api/merchants/:id', async (req, res) => {
+    const response = await merchant_model.deleteMerchant(req.params.id)
+    try {
+        res.status(200).send(response)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+app.listen(config.PORT, () => {
+  console.log(`Server listenning on port ${config.PORT}`);
+});
